8f64e3276bc57d6d23b5620083f5155b9fdd2ed2
Kashish Vaish <kashish.vaish.cs.2018@miet.ac.in>
2020-08-28 15:38:49 +0530

    Uploading Taskmate Project

---
+++ users_app/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+
+class CustomRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    first_name = forms.CharField(max_length=20, required=True)
+    last_name = forms.CharField(max_length=20, required=True)
+
+    class Meta:
+        model = User
+        fields=['username', 'first_name', 'last_name', 'email', 'password1', 'password2']
